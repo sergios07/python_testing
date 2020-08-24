1930486c9a70a351d7bc548c1f53dd457a3fc6e2
sergios07 <63374160+sergios07@users.noreply.github.com>
2020-08-24 22:27:27 +0300

    new information

---
+++ test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+import time
+
+driver=webdriver.Chrome(executable_path="C:\Drivers\Drivers\chromedriver_win32\chromedriver.exe")
+driver.get("")
+driver.maximize_window()
+sdsdgsdg

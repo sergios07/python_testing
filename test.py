dcf54b555dd30f43f63f6ab14d3d35c65b7861ac
sergios07 <63374160+sergios07@users.noreply.github.com>
2020-09-09 00:39:47 +0300

    all project

--- test.py
+++ test.py
@@ -10,35 +10,43 @@ import unittest, time, re
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path="C:\Drivers\Drivers\chromedriver_win32\chromedriver.exe")
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Chrome(executable_path="C:\Drivers\Drivers\chromedriver_win32\chromedriver.exe")
+        self.wd.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
 
     def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("http://test1.ru/")
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("dgdfgdfg")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("dgdfgdfgdfg")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("dgdfgdfgdfgdfgdfg")
-        driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-        driver.find_element_by_link_text("Logout").click()
+        wd = self.wd
+        #open home page
+        wd.get("http://test1.ru/")
+        #login
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        #open groups page
+        wd.find_element_by_link_text("groups").click()
+        #init group creation
+        wd.find_element_by_name("new").click()
+        #fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("dgdfgdfg")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("dgdfgdfgdfg")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("dgdfgdfgdfgdfgdfg")
+        #submit group creation
+        wd.find_element_by_name("submit").click()
+        #return to group page
+        wd.find_element_by_link_text("group page").click()
+        #logout
+        wd.find_element_by_link_text("Logout").click()
 
     # def is_element_present(self, how, what):
     #     try:
@@ -67,7 +75,8 @@ class UntitledTestCase(unittest.TestCase):
     #         self.accept_next_alert = True
 
     def tearDown(self):
-        self.driver.quit()
+        wd = self.wd
+        wd.quit()
         # self.assertEqual([], self.verificationErrors)
 
 

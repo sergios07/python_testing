5742f0cbda98573ac526c24a010a7fdf99851814
sergios07 <63374160+sergios07@users.noreply.github.com>
2020-09-09 14:55:19 +0300

    all project

--- test.py
+++ test.py
@@ -1,89 +1,28 @@
 # -*- coding: utf-8 -*- #используется в том случае если есть текст на русском языке
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+
+import time, re
 from group import Group
+from application import Application
+import pytest
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome(executable_path="C:\Drivers\Drivers\chromedriver_win32\chromedriver.exe")
-        self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+@pytest.fixture() #эта метка превращает фикстуру в инициализатор фикстуры
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+#теперь можено избавиться от тестового класса и фикстуры завершения
 
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.creat_group(wd, Group(name="dgdfgdfg", header="dgdfgdfgdfg", footer="dgdfgdfgdfgdfgdfg"))
-        self.submit_group_creation(wd)
-        self.retur_to_group_page(wd)
-        self.logout(wd)
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.creat_group(Group(name="dgdfgdfg", header="dgdfgdfgdfg", footer="dgdfgdfgdfgdfgdfg"))
+    app.logout()
 
-    # def test_add_empty_group(self):
-    #     wd = self.wd
-    #     self.open_home_page(wd)
-    #     self.login(wd, username="admin", password="secret")
-    #     self.open_group_page(wd)
-    #     self.creat_group(wd, Group(name="", header="", footer=""))
-    #     self.submit_group_creation(wd)
-    #     self.retur_to_group_page(wd)
-    #     self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def retur_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def submit_group_creation(self, wd):
-        wd.find_element_by_name("submit").click()
-
-    def creat_group(self, wd, group):#name, header, footer=group
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://test1.ru/")
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.creat_group(Group(name="", header="", footer=""))
+    app.logout()
 
 
 
-    def tearDown(self):
-        wd = self.wd
-        wd.quit()
-        # self.assertEqual([], self.verificationErrors)
-
-
-if __name__ == "__main__":
-    unittest.main()
 
